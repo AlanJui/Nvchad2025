a1bd516a8b595c46743e9ed898a1b19b17784d43
居正中 <AlanJui.1960@gmail.com>
2025-03-04 15:58:42 +0800

    試著安裝 Markdown Preview

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,21 @@
 return {
+  -- install with yarn or npm
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
+  -- {
+  --   "iamcco/markdown-preview.nvim",
+  --   build = function()
+  --     vim.fn["mkdp#util#install"]()
+  --   end,
+  --   ft = { "markdown" },
+  -- },
   {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
@@ -22,4 +39,5 @@ return {
   -- 		},
   -- 	},
   -- },
+  -- 
 }

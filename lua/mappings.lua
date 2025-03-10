891f2a3d76f51c145cf1dbc47d0e3651a2948bf0
居正中 <AlanJui.1960@gmail.com>
2025-03-10 20:46:02 +0800

    自訂編譯功能

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -193,3 +193,32 @@ map("n", "<leader>l", "<cmd>:Lazy<cr>", { desc = "Lazy" })
 
 -- Terminal toggle options
 map("t", "<esc><esc>", "<c-\\><c-n>", { desc = "Enter Normal Mode" })
+--------------------------------------------------------------------
+-- Run Compiler
+--------------------------------------------------------------------
+map("n", "<leader>rn", function()
+  local is_win = (vim.fn.has "win32" == 1 or vim.fn.has "win64" == 1)
+  local sub_dir = is_win and "\\" or "/"
+
+  -- local file_name = vim.api.nvim_buf_get_name(0)
+  local main_file_name = vim.fn.expand "%:t:r"
+  local file_name = vim.fn.expand "%:t"
+  local file_type = vim.bo.filetype
+  -- print(file_name, file_type)
+  if file_type == "lua" then
+    vim.cmd(":terminal lua " .. file_name)
+  elseif file_type == "python" then
+    vim.cmd(":terminal python " .. file_name)
+  elseif file_type == "c" then
+    vim.cmd(":terminal gcc " .. file_name)
+  elseif file_type == "cpp" then
+    if is_win then
+      -- g++ -g -o %:t:r.exe %:t && ./%:t:r.exe
+      vim.cmd [[terminal g++ -g -o %:t:r.exe %:t && ./%:t:r.exe]]
+      vim.cmd [[":terminal g++ -g -o " .. main_file_name .. ".exe " .. file_name .. " ; ." .. sub_dir .. main_file_name .. ".exe"]]
+    else
+      -- g++ -g -o %:t:r %:t && ./%:t:r
+      vim.cmd(":terminal g++ -g -o %:t:r %:t && ." .. sub_dir .. "%:t:r")
+    end
+  end
+end, { desc = "Run Compiler" })
